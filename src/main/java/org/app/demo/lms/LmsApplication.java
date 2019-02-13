13885f28aed9560e59d3c4d53c0c5dc38d94309a
Shariq Rizvi <shariq@clara.doctor>
2019-02-13 23:09:39 +0500

    API requests logged to file functionality

--- src/main/java/org/app/demo/lms/LmsApplication.java
+++ src/main/java/org/app/demo/lms/LmsApplication.java
@@ -2,10 +2,22 @@ package org.app.demo.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class LmsApplication {
 
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setIncludeHeaders(false);
+	    return loggingFilter;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 	}
--- src/main/java/org/app/demo/lms/rest/UserRestController.java
+++ src/main/java/org/app/demo/lms/rest/UserRestController.java
@@ -2,8 +2,6 @@ package org.app.demo.lms.rest;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +19,6 @@ import org.app.demo.lms.entity.User;
 @RequestMapping("/api")
 public class UserRestController {
 
-    private static final Logger log = LoggerFactory.getLogger(UserRestController.class);
-
 	private UserService userService;
 	
 	@Autowired
@@ -33,7 +29,6 @@ public class UserRestController {
 	// expose "/users" and return list of users
 	@GetMapping("/users")
 	public List<User> findAll() {
-		log.info("Info log statement for LoggingDemoController");
 		return userService.findAll();
 	}
 
